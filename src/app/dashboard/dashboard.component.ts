7183d12d84bc3d327eb9f9501b58fa6410e4cdb6
jpenariasholberton <1875@holbertonschool.com>
2021-07-05 17:25:15 -0500

    Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Dashboard } from '../models/dashboard';
 
+
+const ELEMENT_DATA: Dashboard =
+  { numeroTutelas: 6659,
+    numeroTutelasSinImpugnacion: 3224,
+    numeroTutelasConImpugnacion: 3425,
+    promedios: [{numeroDias: 9, etapa: "radicada"}],
+    tutelasArchivadas: [{
+      cliente: {nombre: "Coomeva", id: null, email: "", direccion:"", fechaCreacion: null},
+      tutela: {idTutela: 12, hecho: "", peticiones: "", termino: null, etapa: "", cliente:null},
+      fechaRegistro: new Date(),
+      investigador: "string",
+      radicador: "string"}]
+  };
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +21,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['cliente', 'idTutela', 'fechaDeRegistro', 'investigador', 'radicador'];
+  displayedColumns2: string[] = ['etapa', 'numeroDias'];
+  data: Dashboard;
+
+  constructor() {
+    this.data=ELEMENT_DATA;
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/promedio.ts
@@ -0,0 +1,4 @@
+export interface Promedio {
+  numeroDias: number;
+  etapa: string;
+}
